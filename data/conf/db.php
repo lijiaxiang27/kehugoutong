326115286f45bb1ae5846d6c5fa72e2778733fec
张世彪 <645702352@qq.com>
2017-08-29 14:08:55 +0800

    添加校区前端ok

--- data/conf/db.php
+++ data/conf/db.php
@@ -5,9 +5,9 @@
 return array(
     'DB_TYPE' => 'mysql',
     'DB_HOST' => 'localhost',
-    'DB_NAME' => 'weixin',
+    'DB_NAME' => 'kehuonline',
     'DB_USER' => 'root',
-    'DB_PWD' => 'mala_xx3',
+    'DB_PWD' => 'root',
     'DB_PORT' => '3306',
     'DB_PREFIX' => 'cmf_',
     //密钥
--- data/runtime/Data/_fields/weixin.cmf_act_user.php
+++
@@ -1,1 +0,0 @@
-a:8:{i:0;s:2:"id";i:1;s:6:"act_id";i:2;s:6:"userid";i:3;s:3:"num";i:4;s:8:"comments";i:5;s:4:"time";s:3:"_pk";s:2:"id";s:5:"_type";a:6:{s:2:"id";s:7:"int(11)";s:6:"act_id";s:7:"int(11)";s:6:"userid";s:7:"int(11)";s:3:"num";s:6:"int(5)";s:8:"comments";s:12:"varchar(500)";s:4:"time";s:12:"varchar(255)";}}
